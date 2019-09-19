8986ed72623a689bb9cc449fce1d028ec7d80c3e
Sarah Munir Jumah <sarahmunirj@gmail.com>
2019-09-19 12:58:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from lane_detection import lane_detection
 
-img = cv2.imread('test_images/test3.jpg')
+img = cv2.imread('~/home/hackathon/Curved-Lane-Lines/test_images/test3.jpg')
 plt.imshow(img)
 #size = (100,100)
 lane_detection(img)
