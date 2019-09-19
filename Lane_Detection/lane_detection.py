5cec92eece5fa81b5219b28c9eddddf92e19cbd9
Abdelrahman-ElGammal <a.elgammal2019@gmail.com>
2019-09-19 15:10:22 +0200

    Update lane_detection.py

--- Lane_Detection/lane_detection.py
+++ Lane_Detection/lane_detection.py
@@ -18,9 +18,9 @@ def lane_detection(img, size = (100,100)):
   out_img, curves, lanes, ploty = sliding_window(img_warp, draw_windows=False)
   time_4 = time.time()
   
-  print("time_color_channel_conv = " + str(int((time_1-time_start)*1000)) + " pipeline_time = "\
-        + str(int((time_2-time_1)*1000)) + " perspective_warp_time = " \
-        + str(int((time_3-time_2)*1000)) + "sliding_window_time = " + str(int((time_4-time_3)*1000)))
-  print("Overall Time = " +str(int((time_4-time_start)*1000)))
+  print("\n time_color_channel_conv = " + str(int((time_1-time_start)*1000)) + "\n pipeline_time = "\
+        + str(int((time_2-time_1)*1000)) + "\n perspective_warp_time = " \
+        + str(int((time_3-time_2)*1000)) + "\n sliding_window_time = " + str(int((time_4-time_3)*1000)))
+  print("\n Overall Time = " +str(int((time_4-time_start)*1000)))
               
   return curves, lanes, ploty, out_img
